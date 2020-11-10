b3342d33bd9106c0194674a8338b911d945553fd
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2020-11-09 19:08:55 -0800

    Added more personal photos, created links for my bio to linkedin, github, and email, improved header section

--- src/Pages/AboutSection/Bio.js
+++ src/Pages/AboutSection/Bio.js
@@ -4,6 +4,9 @@ import "../../style/Bio.css";
 import { Row, Col } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Resume from '../../assets/JosephMartinezResume.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Bio() {
   const [isDesktop, setDesktop] = useState(window.innerWidth > 1024);
@@ -37,8 +40,19 @@ function Bio() {
             In my free time I like to program small video games and continue learning programming through game design. 
             I also enjoy music, and play drums, and I am currently learning guitar and piano.
           </p>
-          <div>
+          <div id="Personal-Links">
             <Button id="Resume-Button" size="md" href={Resume}> Resume </Button> 
+            <div id="Social-Links">
+              <a href="https://github.com/joeymichaelmartinez">
+                <FontAwesomeIcon icon={faGithub} className="Social-Icon"/>
+              </a>
+              <a href="https://www.linkedin.com/in/joeymichaelmartinez/">
+                <FontAwesomeIcon icon={faLinkedin} className="Social-Icon"/>
+              </a>
+              <a href="mailto:joeymichaelmartinez@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} className="Social-Icon"/>
+              </a>
+            </div>
           </div>
           </Col>
       </Row>
--- src/Pages/ProjectsSection/Projects.js
+++ src/Pages/ProjectsSection/Projects.js
@@ -2,63 +2,60 @@ import React, { useState, useEffect } from 'react';
 
 import ProjectCards from './ProjectCard';
 import '../../style/Projects.css';
-import lookingAtComputerStock from '../../assets/images/lookingAtComputerStock.jpg';
+
 import Pokeball from '../../assets/images/Pokeball.svg';
 import DatabaseIcon from '../../assets/images/DatabaseIcon.png';
 import MazeIcon from '../../assets/images/block-maze.svg';
 import UsabilityStudy from '../../assets/images/Usability-Test.svg';
-import BinaryTree from '../../assets/images/binary-tree.svg';
+import Search from '../../assets/images/search.svg';
 import J4Icon from '../../assets/images/J4-Icon.png';
 
 const projectInfo = {
   project1: {
     title: `Web Front-end API`,
-    description: `A simple front-end design to demonstrate integration with a pre-existing API`,
+    description: `A front-end design used to demonstrate integration with a pre-existing API`,
     fullDescription: `Constructed front-end web apps that used a pre-existing web 
                       service API to learn best practices for UI design`,
-    image: UsabilityStudy,
+    image: Pokeball,
   },
   project2: {
     title: `AudiophyleDB`,
     description: `A MySQL database used to store audio files`,
-    fullDescription: `A MySQL database to store audio files and related information in a normalized 
-                      database with CRUD functionality. Set up a simple front-end to handle 
-                      accessing and manipulating this information`,
+    fullDescription: `A database project meant to demonstrate how to store audio files and related 
+                      information in a normalized MySQL database with CRUD functionality. Set up a 
+                      simple front-end to handle accessing and manipulating this information`,
     image: DatabaseIcon,
   },
   project3: {
-    title: `AudiophyleDB`,
-    description: `A MySQL database used to store audio files`,
-    fullDescription: `A MySQL database to store audio files and related information in a normalized 
-                      database with CRUD functionality. Set up a simple front-end to handle 
-                      accessing and manipulating this information`,
-    image: Pokeball,
+    title: `Usability Study`,
+    description: `A study that weighs how well mobile apps handle user experience and interface design`,
+    fullDescription: `A study meant to gather statistics that would inform the overall effectiveness of 
+                      various mobile apps based on certain usability metrics`,
+    image: UsabilityStudy,
   },
   project4: {
-    title: `Web Front-end API`,
-    description: `A simple front-end design to demonstrate integration with a pre-existing API`,
-    fullDescription: `Constructed front-end web apps that used a pre-existing web 
-                      service API to learn best practices for UI design`,
+    title: `Chromophony`,
+    description: `A VR music visualizer built in Unity`,
+    fullDescription: `My second semester Senior year project at LMU. This project was meant to both demonstrate 
+                      the power Unity has in turning music into a visual experience, but also to showcase how 
+                      engaging those visuals can be wgeb experienced through Virtual Reality`,
     image: MazeIcon,
   },
   project5: {
-    title: `Web Front-end API`,
-    description: `A simple front-end design to demonstrate integration with a pre-existing API`,
-    fullDescription: `Constructed front-end web apps that used a pre-existing web 
-                      service API to learn best practices for UI design`,
-    image: BinaryTree,
+    title: `Autocomplete`,
+    description: `A demonstration of how autocomplete is implemented using ternary trees`,
+    fullDescription: `While learning Java and Data Structures, I put together a project that would demonstrate how 
+                      ternary trees are the perfect candidate to implement the common tasks necessary to perform autocomplete`,
+    image: Search,
   },
   project6: {
-    title: `Web Front-end API`,
-    description: `A simple front-end design to demonstrate integration with a pre-existing API`,
-    fullDescription: `Constructed front-end web apps that used a pre-existing web 
-                      service API to learn best practices for UI design`,
+    title: `J4 Lang`,
+    description: `A scripting language meant to emphasize readability and understandability`,
+    fullDescription: `J4 is a scripting language built for LMU's compilers course. It is meant to simplify basic coding constructs 
+                      so that they are more readable and more easily understood. It is a statically and strongly typed language based
+                      on Python and Javascript`,
     image: J4Icon,
   }
-}
-
-const projectImage = {
-  project1: lookingAtComputerStock
 }
 
 function Projects() {
@@ -74,13 +71,15 @@ function Projects() {
   });
 
   return (
-    <div id="Projects">
-      <ProjectCards isDesktop={isDesktop} title={projectInfo.project1.title} fullDescription={projectInfo.project1.fullDescription} description={projectInfo.project1.description} image={projectInfo.project1.image}/>
-      <ProjectCards isDesktop={isDesktop} title={projectInfo.project2.title} fullDescription={projectInfo.project2.fullDescription} description={projectInfo.project2.description} image={projectInfo.project2.image}/>
-      <ProjectCards isDesktop={isDesktop} title={projectInfo.project3.title} fullDescription={projectInfo.project3.fullDescription} description={projectInfo.project3.description} image={projectInfo.project3.image}/>
-      <ProjectCards isDesktop={isDesktop} title={projectInfo.project4.title} fullDescription={projectInfo.project4.fullDescription} description={projectInfo.project4.description} image={projectInfo.project4.image}/>
-      <ProjectCards isDesktop={isDesktop} title={projectInfo.project5.title} fullDescription={projectInfo.project5.fullDescription} description={projectInfo.project5.description} image={projectInfo.project5.image}/>
-      <ProjectCards isDesktop={isDesktop} title={projectInfo.project6.title} fullDescription={projectInfo.project6.fullDescription} description={projectInfo.project6.description} image={projectInfo.project6.image}/>
+    <div id="Projects-Container">
+      <div id="Projects">
+        <ProjectCards isDesktop={isDesktop} title={projectInfo.project1.title} fullDescription={projectInfo.project1.fullDescription} description={projectInfo.project1.description} image={projectInfo.project1.image}/>
+        <ProjectCards isDesktop={isDesktop} title={projectInfo.project2.title} fullDescription={projectInfo.project2.fullDescription} description={projectInfo.project2.description} image={projectInfo.project2.image}/>
+        <ProjectCards isDesktop={isDesktop} title={projectInfo.project3.title} fullDescription={projectInfo.project3.fullDescription} description={projectInfo.project3.description} image={projectInfo.project3.image}/>
+        <ProjectCards isDesktop={isDesktop} title={projectInfo.project4.title} fullDescription={projectInfo.project4.fullDescription} description={projectInfo.project4.description} image={projectInfo.project4.image}/>
+        <ProjectCards isDesktop={isDesktop} title={projectInfo.project5.title} fullDescription={projectInfo.project5.fullDescription} description={projectInfo.project5.description} image={projectInfo.project5.image}/>
+        <ProjectCards isDesktop={isDesktop} title={projectInfo.project6.title} fullDescription={projectInfo.project6.fullDescription} description={projectInfo.project6.description} image={projectInfo.project6.image}/>
+      </div>
     </div>
   )
 }
